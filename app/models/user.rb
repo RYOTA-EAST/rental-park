e7f8b237bd009d2bb098d97b38550c3da1454103
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-03-07 11:16:12 +0900

    adminログイン、ログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
