aa4f9d2e6e0dad106404498cd875f1196d11d582
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-03-14 19:41:29 +0900

    画像をプレビュー表示して保存

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('../jquery.jpostal')
 require('../test')
 
 
@@ -24,4 +23,3 @@ require('../test')
 //= require bootstrap-sprockets
 //= require ../jquery.jpostal 
 //= require ../test
-// require_tree .
