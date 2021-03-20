b19ec21f54a5d8dcc0edb5d434c44896cec64e5f
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-03-21 02:05:00 +0900

    駐車上画像保存、編集

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -71,7 +71,7 @@ class ParksController < ApplicationController
   private
 
   def park_params
-    params.require(:park).permit(:name, :number, :postal_code, :prefecture_code, :city, :street, :explosive, :unit_price, :start_time, :end_time).merge(user_id: current_user.id)
+    params.require(:park).permit(:name, :number, :postal_code, :prefecture_code, :city, :street, :explosive, :unit_price, :start_time, :end_time,:park_image).merge(user_id: current_user.id)
   end
   
   def set_park_params
