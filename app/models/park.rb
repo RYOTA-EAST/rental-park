c6da0acdc7d7e5794afdeead9380f1dd50352ced
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-03-27 11:50:15 +0900

    エラーメッセージ日本語化

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,6 +2,25 @@ class Park < ApplicationRecord
   belongs_to :user
   has_many :events
   has_one_attached :park_image
+
+  with_options presence: true do
+    validates :prefecture_id
+    validates :city
+    validates :street
+    validates :name
+    validates :unit_price
+  end
+
+  validate :start_end_check
+  validate :start_check
+
+  def start_end_check
+    errors.add(:end_time, "は開始時刻より遅い時間を選択してください") if self.start_time >= self.end_time
+  end
+  def start_check
+    errors.add(:start_time, "は現在の日時より遅い時間を選択してください") if self.start_time < Time.now
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
